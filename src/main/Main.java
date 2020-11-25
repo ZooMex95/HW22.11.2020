9153a8a097eef542a1574719832be7daa7caeb12
Evegenii Tolmachev <zoomex95@gmail.com>
2020-11-25 03:28:53 +0300

    1

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,20 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
 public class Main {
+    public static void main(String[] args) {
+        HashMap<String, Integer> mapOfWords = new HashMap<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите имя файла или полный путь");
+        try {
+            String path = reader.readLine();
+        }
+        catch (IOException e) {
+            System.out.println("h1");
+        }
+    }
 }
